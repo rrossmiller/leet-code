a0b3ddbeb499295badb0f4cd2bf5a2b336e8fd0a
Rob Rossmiller <rrossmiller3@gmail.com>
2023-08-14 23:51:43 -0400

    longest common prefix

--- rust/src/longest_common_prefix.rs
+++ rust/src/longest_common_prefix.rs
@@ -2,7 +2,29 @@ struct Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        String::from("")
+        if strs[0] == "" {
+            return String::from("");
+        } else if strs.len() == 1 {
+            return strs[0].clone();
+        }
+        let mut i = 0;
+        let mut is_common = true;
+
+        // while the starting letters of the other words are same as the first
+        while is_common {
+            for j in 1..strs.len() {
+                if strs[j].chars().count() == i + 1
+                    || strs[0].chars().count() == i + 1
+                    || &strs[0][0..i + 1] != &strs[j][0..i + 1]
+                {
+                    is_common = false;
+                    break;
+                }
+            }
+            i += 1;
+        }
+
+        String::from(&strs[0][0..i - 1])
     }
 }
 
@@ -19,6 +41,6 @@ mod tests {
         ];
 
         let r = Solution::longest_common_prefix(i);
-        assert_eq!(r,"fl");
+        assert_eq!(r, "fl");
     }
 }
